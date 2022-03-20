698504e0b6fe7f141d77c9fbd494fe1a21d77d7a
Pavlo Hrendzha <pashahrendzha99@gmail.com>
2022-03-20 18:58:29 +0200

    change port

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const app = require("../app");
 
-const { PORT = 3000, DB_HOST } = process.env;
+const { PORT = 4000, DB_HOST } = process.env;
 
 const connection = mongoose
   .connect(DB_HOST)
